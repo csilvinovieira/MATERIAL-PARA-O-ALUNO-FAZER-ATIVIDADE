dc2d29f927cddc57ca6c0de8b4acd6beda898a02
csilvinovieira <csilvinovieira@gmail.com>
2024-10-08 01:25:12 -0300

    primeiro commit

--- Repositories/ProjetoRepository.cs
+++ Repositories/ProjetoRepository.cs
@@ -4,18 +4,47 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+
 namespace Exo.WebApi.Repositories
 {
     public class ProjetoRepository
     {
-        private readonly ExoContext _context;
-        public ProjetoRepository(ExoContext context)
+    private readonly ExoContext _context;
+    public ProjetoRepository(ExoContext context)
+    {
+    _context = context;
+    }
+    public List<Projeto> Listar()
+    {
+    return _context.Projetos.ToList();
+    }
+    // Código novo que completa o CRUD.
+    public void Cadastrar(Projeto projeto)
+    {
+        _context.Projetos.Add(projeto);
+        _context.SaveChanges();
+    }
+    public Projeto BuscarporId(int id)
+    {
+        return _context.Projetos.Find(id);
+    }
+    public void Atualizar(int id, Projeto projeto)
+    {
+        Projeto projetoBuscado = _context.Projetos.Find(id);
+        if (projetoBuscado != null)
         {
-        _context = context;
+        projetoBuscado.NomeDoProjeto = projeto.NomeDoProjeto;
+        projetoBuscado.Area = projeto.Area;
+        projetoBuscado.Status = projeto.Status;
         }
-        public List<Projeto> Listar()
-        {
-            return _context.Projetos.ToList();
-        }
+        _context.Projetos.Update(projetoBuscado);
+        _context.SaveChanges();
+    }
+    public void Deletar(int id)
+    {
+        Projeto projetoBuscado = _context.Projetos.Find(id);
+        _context.Projetos.Remove(projetoBuscado);
+        _context.SaveChanges();
+    }
     }
 }
--- obj/Debug/net6.0/Exo.WebApi.AssemblyInfo.cs
+++ obj/Debug/net6.0/Exo.WebApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Exo.WebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+46aabbd40c66ac41e352403498b52f77c67ac9e8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8b81da5f0d94ec9bf87e669b713c63af3801318f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Exo.WebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Exo.WebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
